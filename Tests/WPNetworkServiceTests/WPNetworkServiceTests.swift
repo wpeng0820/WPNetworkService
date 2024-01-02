4a521dae89482a765b220f4e5f3430a8956c98ce
willPeng <panga0820@gmail.com>
2024-01-02 22:15:12 +0800

    Remove unuse test

--- Tests/WPNetworkServiceTests/WPNetworkServiceTests.swift
+++ Tests/WPNetworkServiceTests/WPNetworkServiceTests.swift
@@ -6,6 +6,6 @@ final class WPNetworkServiceTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(WPNetworkService().text, "Hello, World!")
+        
     }
 }
